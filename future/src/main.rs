95311aeccac674e722985009f2aa952efbb0f74f
gcxfd <i@rmw.link>
2022-06-16 05:35:54 +0800

    🔶

--- future/src/main.rs
+++ future/src/main.rs
@@ -3,6 +3,7 @@
 use std::{
     future::Future,
     pin::Pin,
+    ptr,
     sync::Arc,
     task::{Context, Poll},
     thread::{sleep, spawn},
@@ -40,10 +41,11 @@ impl Future for RecvFuture {
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let state = &self.state;
         state.waker.register(cx.waker());
-        let done = unsafe { std::ptr::read_volatile(&state.done as _) };
+        let done = unsafe { ptr::read_volatile(&state.done as _) };
         if done {
-            let state = unsafe { Arc::get_mut_unchecked(&mut self.get_mut().state) };
-            Poll::Ready(state.msg.take())
+            //let state = unsafe { Arc::get_mut_unchecked(&mut self.get_mut().state) };
+            let msg = unsafe { ptr::read_volatile(&state.msg as *const Msg) };
+            Poll::Ready(msg)
         } else {
             Poll::Pending
         }
@@ -55,19 +57,22 @@ async fn recv() -> Option<Box<[u8]>> {
     let mut state = future.state.clone();
     spawn(move || {
         sleep(Duration::from_secs(1));
+        #[allow(unused_mut)]
         let mut state = unsafe { Arc::get_mut_unchecked(&mut state) };
-        state.msg = Some(Box::new([1, 2, 3]));
         unsafe {
-            std::ptr::write_volatile(&mut state.done as _, true);
+            ptr::write_volatile(&mut state.msg as *mut Msg, Some(Box::new([1, 2, 3])));
+            ptr::write_volatile(&mut state.done as _, true);
         }
         state.waker.wake();
     });
+    /*
     let state2 = future.state.clone();
     spawn(move || {
         sleep(Duration::from_secs(2));
         dbg!("wake again");
         state2.waker.wake();
     });
+    */
     future.await
 }
 
